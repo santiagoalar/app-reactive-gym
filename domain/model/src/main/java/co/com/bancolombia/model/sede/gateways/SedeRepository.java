f57b2fe20b647f3c9a49560f3ad7cba9a3f57ed8
SergioAPineda <sergio.pinedas94@gmail.com>
2021-09-21 08:29:14 -0500

    Se crean los casos de uso para sede.

--- domain/model/src/main/java/co/com/bancolombia/model/sede/gateways/SedeRepository.java
+++ domain/model/src/main/java/co/com/bancolombia/model/sede/gateways/SedeRepository.java
@@ -1,4 +1,13 @@
 package co.com.bancolombia.model.sede.gateways;
 
+import co.com.bancolombia.model.sede.Sede;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface SedeRepository {
+    Flux<Sede> getAll();
+    Mono<Sede>  getSedeById(String id);
+    Mono<Sede> createSede(Sede sede);
+    Mono<Sede> updateSede(String id, Sede sede);
+    Mono<Void> deleteSede(String id);
 }
